705c2705f12e0afd013adfbfd8a70e57107f0c3f
Steven Lu (PuTTY Win7 on Centos 7 hydra1[tmux]) <stevenlu443+hydra1@gmail.com>
2016-12-08 22:33:40 -0500

    an init script for zsh plugins

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run me to init the extra plugin repos. i'm doing it in a dumb sub dir 
+# checkout approach
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
