14ffd8d9532426716a72d464e4bc128ceaa1c94c
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-09-20 04:03:04 -0400

    still troubleshooting. seems like gpt4 got me here to a working impl but doesnt really understand how it works to fix the edge case so i'll have to root it out.

--- line-editor.zsh
+++ line-editor.zsh
@@ -3,7 +3,7 @@ function move-current-arg-left {
   local buffer="${LBUFFER}${RBUFFER}"
   printf "buf: >>%q<<\n" "$buffer" >> ~/zsh_word_splitting_log.txt
   args=(${(z):-"$buffer"})
-  
+
   # Finding the index of the current argument based on the cursor position
   local idx
   local length=0
@@ -15,7 +15,7 @@ function move-current-arg-left {
       break
     fi
   done
-  
+
   # Finding the relative cursor position within the current argument
   local cursor_pos_within_arg=$(( ${#LBUFFER} - $length + ${#args[idx]} + 1 ))
 
@@ -24,13 +24,12 @@ function move-current-arg-left {
     local temp=$args[$idx]
     args[$idx]=$args[$((idx-1))]
     args[$((idx-1))]=$temp
-    
+
     # Finding the new cursor position
     local new_cursor_pos=0
     for i in {1..$((idx-2))}; do
       new_cursor_pos=$(( $new_cursor_pos + ${#args[i]} + 1 ))
     done
-    
     new_cursor_pos=$(( $new_cursor_pos + cursor_pos_within_arg ))
 
     # Reconstructing LBUFFER and RBUFFER
@@ -39,6 +38,7 @@ function move-current-arg-left {
     RBUFFER=" ${(j: :)args[idx+1,-1]}"
     CURSOR=$new_cursor_pos
   fi
+
 }
 
 zle -N move-current-arg-left
