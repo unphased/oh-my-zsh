c056aa416d00a8cf3e0a55e6ce452897355656b3
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2025-06-11 16:00:37 -0400

    - old bugs fixed - new bugs added

--- term-capture/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_RUNNER
-#include "catch.hpp"
-
-int main(int argc, char* argv[]) {
-    // Global setup can go here
-    int result = Catch::Session().run(argc, argv);
-    // Global cleanup can go here
-    return result;
-}
