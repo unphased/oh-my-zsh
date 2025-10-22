f24f79ae7d130e830141bac9b0eb87d12a8d3dbc
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2025-10-22 19:46:48 -0400

    fix line editor

--- line-editor.zsh
+++ line-editor.zsh
@@ -1,89 +1,92 @@
-function move-current-arg-left {
-  move-current-arg-direction left
-}
-function move-current-arg-right {
-  move-current-arg-direction right
-}
+# Move the argument under the cursor left or right using zsh's shell-aware splitter.
+autoload -Uz split-shell-arguments
 
-function move-current-arg-direction {
-  local -a args
-  local buffer="${LBUFFER}${RBUFFER}"
-  echo ====== >> ~/zsh_word_splitting_log.txt
-  printf "start: >>%s█%s<<\n" "$LBUFFER" "$RBUFFER" >> ~/zsh_word_splitting_log.txt
-  printf "cursor: $CURSOR ${#LBUFFER}\n" >> ~/zsh_word_splitting_log.txt
-  args=(${(z):-"$buffer"})
-  local -a separators
-  local last_idx=$(( ${#args[1]} + 1 ))
-  unset CURSORIDX
+__le_move_arg() {
+  emulate -L zsh
+  local direction=$1
 
-  for arg in "${args[@]:1}"; do
-    # Finding the start index of the current argument in the buffer
-    # printf "Scanning this region now: >>%s<<\n" "${buffer[last_idx,-1]}" >> ~/zsh_word_splitting_log.txt
-    local start_idx=$(( ${buffer[last_idx,-1][(i)$arg]} + last_idx - 1 ))
+  split-shell-arguments || { zle beep; return 0 }
 
-    separators+="${buffer[last_idx,start_idx-1]}"
-    # printf "Adding separator in spot %d built from idxs %d to %d: >>%q<<\n" "${#separators}" "$last_idx" "$start_idx" "${buffer[last_idx,start_idx-1]}" >> ~/zsh_word_splitting_log.txt
+  local -a parts
+  parts=("${reply[@]}")
+  local -i part_count=${#parts}
+  if (( part_count < 3 )); then
+    zle beep
+    return 0
+  fi
 
-    if (( CURSOR >= $(( start_idx - 1)) && CURSOR < ( start_idx + ${#arg} ) )); then
-      CURSORIDX=$(( ${#separators} + 1 ))
-      CURSOROFFSET=$(( $CURSOR - $start_idx + 2 ))
-      printf "##### Cursor ($CURSOR) is associated with this arg (range $start_idx ~ $(( start_idx + ${#arg} ))) (no. %d): >>%s<<, the offset within the arg is %d\n" "$CURSORIDX" "$arg" "$CURSOROFFSET" >> ~/zsh_word_splitting_log.txt
+  local -i segment_index=$REPLY
+  local -i cursor_suboffset=$(( REPLY2 - 1 ))
+  (( cursor_suboffset < 0 )) && cursor_suboffset=0
+
+  local -i word_index=0
+  if (( segment_index % 2 == 0 )); then
+    word_index=$segment_index
+  else
+    if (( part_count == 1 )); then
+      zle beep
+      return 0
+    elif (( segment_index == 1 )); then
+      word_index=2
+    elif (( segment_index == part_count )); then
+      word_index=part_count-1
+    else
+      word_index=segment_index+1
     fi
+    cursor_suboffset=0
+  fi
 
-    # Setting the last index to after the end of the current argument
-    last_idx=$(( start_idx+${#arg} ))
-    printf "last_idx set now to %d after incrementing startidx=%d by %d >>%s<<\n" "$last_idx" "$start_idx" ${#arg} "$arg" >> ~/zsh_word_splitting_log.txt
+  if (( word_index <= 1 || word_index >= part_count )); then
+    zle beep
+    return 0
+  fi
+
+  local -i target_index
+  if [[ $direction == left ]]; then
+    if (( word_index <= 2 )); then
+      zle beep
+      return 0
+    fi
+    target_index=$(( word_index - 2 ))
+  elif [[ $direction == right ]]; then
+    if (( word_index >= part_count - 1 )); then
+      zle beep
+      return 0
+    fi
+    target_index=$(( word_index + 2 ))
+  else
+    return 1
+  fi
+
+  local tmp=${parts[target_index]}
+  parts[target_index]=${parts[word_index]}
+  parts[word_index]=$tmp
+
+  local new_buffer="${(j::)parts}"
+  local -i new_cursor=0
+  local -i i=1
+  while (( i < target_index )); do
+    (( new_cursor += ${#parts[i]} ))
+    (( ++i ))
   done
 
-  # Assert len of separators is one less than len of args
-  if (( ${#separators} != ${#args} - 1 )); then
-    printf "Error: separators and args are not the appropriate length: %d vs %d\n" "${#separators}" "${#args}" >> ~/zsh_word_splitting_log.txt
-    for arg in "${separators[@]}"; do
-      printf "Separator (escaped dump): >>%q<<\n" "$arg" >> ~/zsh_word_splitting_log.txt
-    done
+  local -i segment_length=${#parts[target_index]}
+  if (( cursor_suboffset > segment_length )); then
+    cursor_suboffset=$segment_length
   fi
+  (( new_cursor += cursor_suboffset ))
 
-  # Printing the args and separators zipped up for debugging
-  for i in {1..$(( ${#args} - 1 ))}; do
-    printf "Arg ($i): >>%s<< Separator (escaped dump): >>%q<<\n" "${args[i]}" "${separators[i]}"
-  done >> ~/zsh_word_splitting_log.txt
-  printf "Last Arg (${#args}): >>%s<<\n\n" "$args[-1]" >> ~/zsh_word_splitting_log.txt
+  BUFFER=$new_buffer
+  CURSOR=$new_cursor
+  LBUFFER=${BUFFER[1,new_cursor]}
+  RBUFFER=${BUFFER[new_cursor+1,-1]}
 
-  # Swapping the current argument with the previous one if it's not the first argument
-  if (( ${+CURSORIDX} )); then
-    printf "Cursor was determined to be on arg number %d at position %d\n" "$CURSORIDX" "$CURSOROFFSET" >> ~/zsh_word_splitting_log.txt
-    # Perform an array element swap
-    if [ $1 == 'left' ]; then
-      TARGETIDX=$(( CURSORIDX - 1 ))
-    else
-      TARGETIDX=$(( CURSORIDX + 1 ))
-    fi
-    local tmp=$args[$CURSORIDX]
-    args[$CURSORIDX]=$args[$TARGETIDX]
-    args[$TARGETIDX]=$tmp
-    # build the buffers again
-    LBUFFER=""
-    RBUFFER=""
-    for i in {1..$(( ${#args} - 1 ))}; do
-      if (( i < TARGETIDX )); then
-        LBUFFER="$LBUFFER${args[i]}${separators[i]}"
-        printf "i=$i Appending L >>%q<<\n" "${args[i]}${separators[i]}">> ~/zsh_word_splitting_log.txt
-      elif (( i == TARGETIDX )); then
-        LBUFFER="$LBUFFER${args[i][1,CURSOROFFSET-1]}"
-        RBUFFER="${args[i][CURSOROFFSET,-1]}${separators[i]}"
-        printf "i=$i Appending L >>%q<< and initiating R >>%q<<\n" "${args[i][1,CURSOROFFSET-1]}" "${args[i][CURSOROFFSET,-1]}${separators[i]}">> ~/zsh_word_splitting_log.txt
-      else
-        RBUFFER="$RBUFFER${args[i]}${separators[i]}"
-        printf "i=$i Appending R >>%q<<\n" "${args[i]}${separators[i]}">> ~/zsh_word_splitting_log.txt
-      fi
-    done
-    RBUFFER="$RBUFFER${args[-1]}"
-    printf "i=$i Appending R >>%q<<\n" "${args[-1]}">> ~/zsh_word_splitting_log.txt
-
-  else
-    printf "Cursor not on an arg, aborting." >> ~/zsh_word_splitting_log.txt
-  fi
+  zle redisplay
+  return 0
 }
+
+move-current-arg-left() { __le_move_arg left }
+move-current-arg-right() { __le_move_arg right }
 
 zle -N move-current-arg-left
 zle -N move-current-arg-right
